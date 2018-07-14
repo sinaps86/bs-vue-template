01e2520fa4f47ba72d7e552b1792de5f373f4c5c
Kotelnikov Nikolay <sinaps1986@gmail.com>
2018-07-14 20:01:52 +0700

    + car

--- src/pages/main-page/script.js
+++ src/pages/main-page/script.js
@@ -9,32 +9,32 @@ export default {
         width: 48, height: 48, x: 884, y: 472, hold: false
       },
       trafic_lights: [
-        {x: 456, y: 397, radius: 100, state: "red"},
-        {x: 510, y: 216, radius: 100, state: "red"},
-        {x: 529, y: 202, radius: 100, state: "red"},
-        {x: 753, y: 398, radius: 100, state: "red"},
-        {x: 606, y: 399, radius: 100, state: "red"},
-        {x: 637, y: 384, radius: 100, state: "red"},
-        {x: 722, y: 498, radius: 100, state: "red"},
-        {x: 807, y: 142, radius: 100, state: "red"},
-        {x: 798, y: 380, radius: 100, state: "red"},
-        {x: 776, y: 567, radius: 100, state: "red"},
-        {x: 794, y: 565, radius: 100, state: "red"},
-        {x: 808, y: 582, radius: 100, state: "red"},
-        {x: 862, y: 23, radius: 100, state: "red"},
-        {x: 888, y: 384, radius: 100, state: "red"},
-        {x: 866, y: 517, radius: 100, state: "red"},
-        {x: 573, y: 396, radius: 100, state: "red"},
-        {x: 988, y: 387, radius: 100, state: "red"},
-        {x: 988, y: 386, radius: 100, state: "red"},
-        {x: 1003, y: 334, radius: 100, state: "red"},
-        {x: 1076, y: 274, radius: 100, state: "red"},
-        {x: 952, y: 606, radius: 100, state: "red"},
-        {x: 974, y: 68, radius: 100, state: "red"},
-        {x: 875, y: 62, radius: 100, state: "red"},
-        {x: 1169, y: 351, radius: 100, state: "red"},
-        {x: 1180, y: 258, radius: 100, state: "red"},
-        {x: 1258, y: 269, radius: 100, state: "red"},
+        {x: 456, y: 397, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 510, y: 216, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 529, y: 202, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 753, y: 398, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 606, y: 399, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 637, y: 384, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 722, y: 498, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 807, y: 142, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 798, y: 380, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 776, y: 567, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 794, y: 565, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 808, y: 582, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 862, y: 23, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 888, y: 384, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 866, y: 517, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 573, y: 396, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 988, y: 387, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 988, y: 386, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 1003, y: 334, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 1076, y: 274, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 952, y: 606, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 974, y: 68, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 875, y: 62, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 1169, y: 351, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 1180, y: 258, width: 10, height: 20, radius: 70, state: "red"},
+        {x: 1258, y: 269, width: 10, height: 20, radius: 70, state: "red"},
       ],
     }
   },
@@ -76,6 +76,8 @@ export default {
 
         this.car.x = x - (this.car_container.width / 2) + this.car.width;
         this.car.y = y - (this.car_container.height / 2) + this.car.height;
+
+        this.checkLights();
       }
     },
     carContainerMove(evt) {
@@ -86,7 +88,20 @@ export default {
 
         this.car.x = x - (this.car.width / 2);
         this.car.y = y - (this.car.height / 2); ///
+
+
       }
+    },
+    checkLights() {
+      this.trafic_lights.forEach(el => {
+        //let hypot = Math.hypot((el.x - el.width/2) - (this.car.x - this.car.width/2), (el.y - el.height/2) - (this.car.y - this.car.height / 2));
+        let hypot = Math.hypot((this.car.x - this.car.width/2) - (el.x - el.width/2), (this.car.y - this.car.height / 2) - (el.y - el.height/2));
+        this.showConsole && console.log("Hypot", hypot);
+        if (hypot < el.radius/2) {
+          this.showConsole && console.log("Попали на светофор");
+          el.state = "green";
+        }
+      })
     }
   }
 }
